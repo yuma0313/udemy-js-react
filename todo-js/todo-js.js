fb90092d2baf3906374b4737031f8691cbedb635
yuma0313 <y.yuma0313@gmail.com>
2023-02-18 19:58:48 +0900

    タスク追加・完了・削除機能

---
+++ todo-js/todo-js.js
@@ -0,0 +1,64 @@
+const onClickAdd = () => {
+  //テキストボックスの値を取得し初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  //buttonタグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了"
+  completeButton.addEventListener("click", () => {
+    //完了ボタン押した後に削除
+    deleteFromIncompleteList(completeButton.parentNode);
+
+    //完了リストに追加する要素
+    const addTarget = completeButton.parentNode;
+
+    //todo内容テキストを取得
+    const text = addTarget.firstElementChild.innerText;
+
+    //div以下を初期化
+    addTarget.textContent = null;
+
+    //liを生成
+    const li = document.createElement("li");
+    li.innerText = text;
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+
+    //addTargetタグに各要素を設定
+    addTarget.appendChild(li);
+    addTarget.appendChild(backButton);
+    //完了リストに追加
+    document.getElementById("complete-list").appendChild(addTarget);
+  })
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除"
+  deleteButton.addEventListener("click", () => {
+  deleteFromIncompleteList(deleteButton.parentNode);
+  });
+
+  //divタグの子要素に各要素を設定
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  //未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+//未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+}
+
+document.getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
+
